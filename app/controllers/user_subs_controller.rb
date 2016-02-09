2d4524e141298ad590b5b64c2c17080add464818
Paul Morana <pmorana@gmail.com>
2016-02-09 11:59:13 -0600

    redir to current sub on subscribe and all subscriptions on unsub

--- app/controllers/user_subs_controller.rb
+++ app/controllers/user_subs_controller.rb
@@ -9,13 +9,13 @@ class UserSubsController < ApplicationController
 	def create
 		sub = Subscription.find(params[:subscription_id])
 		current_user.subscribe(sub)
-		redirect_to root_path
+		redirect_to subscription_path(sub)
 	end
 
 	def destroy
 		#needs to be .subscription at end
 		sub = UserSub.find(params[:id]).subscription
 		current_user.unsubscribe(sub)
-		redirect_to root_path
+		redirect_to subscriptions_path
 	end
 end
